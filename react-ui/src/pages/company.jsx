f1aa618079cbd8a9927537d35e006451a49d4766
Olivier Rheault Gagnon <o.rheaultgagnon@gmail.com>
2021-02-13 01:17:42 -0500

    wip: company page

--- react-ui/src/pages/company.jsx
+++ react-ui/src/pages/company.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 export const Company = (props) => {
+
+
+    /*useEffect(() => {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            //body: JSON.stringify({"symbol": value})
+        };
+
+        fetch('/inside-trader', requestOptions)
+            .then(response => response.json())
+            .then(data => {})
+            .catch(console.log)
+    }, []);
+*/
     return (
-        <div><h3>Company {props.location.state.symbol}</h3></div>
+        <div><h3>Symbol: {props.location.state.symbol}</h3></div>
     );
 };
